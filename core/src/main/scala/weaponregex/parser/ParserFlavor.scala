d97b65f3f8fd9012b4187a1078b921bb2de86a3d
Nhat <nhaajt@gmail.com>
2021-02-15 16:21:02 +0100

    feat(parser-flavor): Export parser flavors

--- core/src/main/scala/weaponregex/parser/ParserFlavor.scala
+++ core/src/main/scala/weaponregex/parser/ParserFlavor.scala
@@ -1,5 +1,12 @@
 package weaponregex.parser
 
+import scala.scalajs.js.annotation._
+
+@JSExportTopLevel("ParserFlavor")
 sealed trait ParserFlavor
+
+@JSExportTopLevel("ParserFlavorJVM")
 case object ParserFlavorJVM extends ParserFlavor
+
+@JSExportTopLevel("ParserFlavorJS")
 case object ParserFlavorJS extends ParserFlavor
